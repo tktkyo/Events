af4642769cf80ba448b16c8e2869ac2f31e61573
kyo taka <tkt@gmail.com>
2025-01-29 15:28:31 +0900

    Events画面にDBからのデータを反映させるようにした
    タイトルの色が黒に…

--- src/main/java/controller/EventsServlet.java
+++ src/main/java/controller/EventsServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dto.Events;
 
@@ -22,25 +21,24 @@ import dto.Events;
 public class EventsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	* @see HttpServlet#doPost(HttpServletRequest request,
-	HttpServletResponse response)
-	*/
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// DB接続設定
-		String url = "jdbc:mysql://127.0.0.1:3306/events?useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Tokyo";
+		String url = "jdbc:mysql://127.0.0.1:3306"
+				+ "/events"
+				+ "?useUnicode=true"
+				+ "&characterEncoding=utf8"
+				+ "&serverTimezone=Asia/Tokyo";
 		String user = "root";
 		String pass = "";
 
 		try (Connection con = DriverManager.getConnection(url, user, pass)) {
-			// DBからデータを取得
-
+			// conを使ってSQLを実行　　DBからデータを取得
 			String sql = "SELECT * FROM events";
 			PreparedStatement stmt = con.prepareStatement(sql);
 			ResultSet rs = stmt.executeQuery();
-
+			
+			
 			Events events = null;
 			if (rs.next()) {
 				Integer id = (Integer) rs.getObject("id");
@@ -62,24 +60,5 @@ public class EventsServlet extends HttpServlet {
 			e.printStackTrace();
 			System.out.println("未接続");
 		}
-	}
-
-	/**
-	* @see HttpServlet#doPost(HttpServletRequest request,
-	HttpServletResponse response)
-	*/
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		//セッションから登録内容を取り出す
-		HttpSession session = request.getSession();
-		Events events = (Events) session.getAttribute("events");
-
-		//セッションの内容が不要になったので破棄する
-		session.invalidate();
-
-		//forward　
-		request.getRequestDispatcher("/WEB-INF/view/events.jsp").forward(request, response);
-
 	}
 }
